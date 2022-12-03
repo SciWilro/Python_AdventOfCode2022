307dbd566f4f43b841a58f6b506aa3dbdaa54af0
SciWilro <mr.wilro@gmail.com>
2022-12-03 03:37:56 +0200

    Alternative answer - Hardcoding

--- Day02.py
+++ Day02.py
@@ -88,6 +88,9 @@ print(f"score: {sum(scores)}")
 
 # Shorter Alternative
 # Hardcode answers in list/dict
+lines = [line.strip() for line in open(inputpath).readlines()]
+answers = {"A X": 3, "A Y": 4, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7}
+print(f"Score For Part 2: {sum(answers[i] for i in lines)}")
 
 # "Better" Alternative
 # Convert to numbers (Modular Arithmetic)
